94558158d9a6f383cf9604e0f7363d06bb48b0ff
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2023-02-03 23:32:54 +0545

    day-03 working with model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Skill(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    no_of_year = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
 # Create your models here.
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Skill
 
 
 from django.views.generic import TemplateView
@@ -6,3 +7,8 @@ from django.views.generic import TemplateView
 
 class IndexPageView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["skill_list"] = Skill.objects.all()
+        return context
